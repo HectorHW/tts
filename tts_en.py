cac84a7d92ff561356a02581e4448e07294054eb
Hector Hanshew <github@hectorhw.net>
2024-08-11 15:24:43 +0300

    use simple command interpreter instead of reading text from args

--- tts_en.py
+++ tts_en.py
@@ -1,29 +1,43 @@
 import silero
-import sys
 from pydub import AudioSegment
 from pydub.playback import play
 import pathlib
 
-text = " ".join(sys.argv[1:]).strip()
-
-if not text:
-    print("please provide non-empty text via arg", file=sys.stderr)
-    sys.exit(1)
+model, _ = silero.silero_tts(language="en", speaker="v3_en")
 
 tmpdir = pathlib.Path.home() / ".tts"
 tmpdir.mkdir(exist_ok=True)
 
-model, _ = silero.silero_tts(language="en", speaker="v3_en")
-
 output_file = tmpdir / "output.wav"
 
-ssml = f"""
-<speak>
-{text}
-<break time="100ms" />
-</speak>"""
 
-model.save_wav(ssml_text=ssml, speaker=f"en_51", audio_path=str(output_file))
+def say_given_text(value: str) -> None:
+    ssml = f"""
+    <speak>
+    {value}
+    <break time="100ms" />
+    </speak>"""
 
-mysong = AudioSegment.from_wav(output_file)
-play(mysong)
+    model.save_wav(ssml_text=ssml, speaker=f"en_51", audio_path=str(output_file))
+
+    mysong = AudioSegment.from_wav(output_file)
+    play(mysong)
+
+input_buffer = ""
+
+while True:
+    try:
+        command = input("tts> ").strip()
+    except EOFError:
+        break
+    match command:
+        case "quit" | "exit":
+            break
+        case "SAY":
+            if not input_buffer:
+                print("no text in buffer!")
+                continue
+            say_given_text(input_buffer)
+            input_buffer = ""
+        case _:
+            input_buffer += " " + command
